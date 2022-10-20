8c5ee95b8b5a727a14ef6e73936c59f156aba684
Vedanshi-Shah <vedanshi283@gmail.com>
2022-10-20 08:28:14 +0530

    added a perfect evaluator

--- bloom_filter/main.c
+++ bloom_filter/main.c
@@ -5,7 +5,7 @@
 #include <inttypes.h>
 int main(){
     parse();
-    tree t=generate(23);
+    tree t=generate(25);
     char **arr=(char **)malloc(sizeof(char *)*10);
     for(int i=0;i<10;i++){
         arr[i]=(char *)malloc(sizeof(char)*100);
